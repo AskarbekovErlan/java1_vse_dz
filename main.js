5062932c103930368069eada5001ca3585217a91
AskarbekovErlan <askarbecoverlan@MacBook-Air-Askarbecov.local>
2022-07-14 23:19:28 +0600

    lesson_3

---
+++ main.js
@@ -0,0 +1,81 @@
+// function sayHello() {
+//     console.log("Hello")
+//
+// }
+// sayHello();
+//
+// function getFullName(user, age) {
+//     if (age < 18){
+//         console.log(user.first_name, user.last_name + " " + "close")
+//     } else {
+//                 console.log(user.first_name, user.last_name + " " + "open")
+//     }
+//
+// }
+// const Name = {
+//     first_name:"Maruf",
+//     last_name:"Bekkeldiev"
+// }
+// const name2 = "text"
+// getFullName(Name, 17, name2)
+//  const anonym = function () {
+//     console.log("teste")
+//  }
+//  anonym()
+// //
+// function numbers(number1, number2) {
+//     if (number1 > number2){
+//         console.log("num1 > num2")
+//     }else if(number1 < number2){
+//         console.log("num1 < num2")
+//     }else {
+//         console.log("===")
+//     }
+// }
+// const num1 = prompt("число1?")
+// const num2 = prompt("число2?")
+// numbers(num1, num2)
+//
+// function twoMass(arr1, arr2) {
+//     if (arr.length > arr2.length){
+//         console.log("arr1 > arr2")
+//     } else if (arr.length < arr2.length){
+//         console.log("arr1 < arr2")
+//     }
+// }
+//  const arr1 = [232, 2342, 454, 34]
+//  const arr2 = [232, 2342, 454, 34, 32, 42, 3232, 324]
+// twoMass(arr1, arr2)
+// //
+// function bt() {
+//     let line = "*"
+//     while (line.length < 7){
+//         console.log(line)
+//         line = line = "*"
+//     }
+// }
+//
+//
+
+
+function numbers(number1, number2) {
+    if (number1 > number2){
+        console.log("num1 > num2")
+    }else if(number1 < number2){
+        console.log("num1 < num2")
+    }else {
+        console.log("===")
+    }
+}
+const num1 = prompt("число1?")
+const num2 = prompt("число2?")
+numbers(num1, num2)
+// // **********
+
+function arg(num){
+    console.log(num.length)
+}
+const num = [5876, 7987, 78]
+arg(num)
+
+
